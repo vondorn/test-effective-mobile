32d489ffcb3fb22ff36082c47e928f2a2020e7cf
vondorn <vondoryn@gmail.com>
2026-05-15 18:42:58 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`[server]: Сервер запущен на http://localhost:${PORT}`);
+});
